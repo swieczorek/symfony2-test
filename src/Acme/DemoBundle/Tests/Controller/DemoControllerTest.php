7a8436f6a0d2a3c8e44978965e70ea82539f98dd
Stefan Wieczorek <stefan.wieczorek@mgt-commerce.com>
2014-01-17 14:25:25 +0100

    .

--- src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
+++ src/Acme/DemoBundle/Tests/Controller/DemoControllerTest.php
@@ -42,4 +42,9 @@ class DemoControllerTest extends WebTestCase
         // check that the page is the right one
         $this->assertCount(1, $crawler->filter('h1.title:contains("secured for Admins only!")'));
     }
+    
+    public function testMuha()
+    {
+        $this->assertCount(1, 1);
+    }
 }
